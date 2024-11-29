63f54c8046966e932fff61673d5aff139c5598b6
Ankit Kumar Sharma <ankit.sharma.cg@hitachi-systems.com>
2024-11-29 15:52:26 +0530

    dashboard integrated

--- code/UI/src/components/userspage/DashboardPage.jsx
+++ code/UI/src/components/userspage/DashboardPage.jsx
@@ -25,42 +25,16 @@ function DashboardPage() {
   return (
     <>
       <div className="dashboardPageContainer">
-        {company == "PNB" ? (
-          <div className="graphContainer">
-            <iframe
-              src="http://13.126.48.191:7000/d-solo/ad7cee07-4fa2-456f-bc37-c804abe9e64c/ubi?orgId=1&refresh=5s&from=1728043405000&to=1728048330000&theme=light&panelId=1"
-              width="100%"
-              height="100%"
-              frameBorder="0"
-            ></iframe>
-            <div className="frameContainer">
-              <div className="frame1">
-                <iframe
-                  src="http://13.126.48.191:7000/d-solo/ad7cee07-4fa2-456f-bc37-c804abe9e64c/ubi?orgId=1&refresh=5s&from=1728043405000&to=1728048330000&theme=light&panelId=3"
-                  width="100%"
-                  height="100%"
-                  frameborder="0"
-                ></iframe>
-              </div>
-              <div className="frame2">
-                <iframe
-                  src="http://13.126.48.191:7000/d-solo/ad7cee07-4fa2-456f-bc37-c804abe9e64c/ubi?orgId=1&refresh=5s&from=1728043405000&to=1728048330000&theme=light&panelId=6"
-                  width="100%"
-                  height="100%"
-                  frameborder="0"
-                ></iframe>
-              </div>
-            </div>
-            <div className="frame3">
-              <iframe
-                src="http://13.126.48.191:7000/d-solo/ad7cee07-4fa2-456f-bc37-c804abe9e64c/ubi?orgId=1&refresh=5s&from=1728043405000&to=1728048330000&theme=light&panelId=2"
-                width="100%"
-                height="100%"
-                frameborder="0"
-              ></iframe>
-            </div>
+        {company == "UBI" ? (
+          <div className="frameContent" style={{"width":"100%",height:"100vh"}}>
+            <iframe src="http://13.126.48.191:7000/d/ad7cee07-4fa2-456f-bc37-c804abe9e64c/ubi?orgId=1&refresh=5s&from=1728043405000&to=1728048330000&theme=light" frameborder="0" width="100%" height="100%" style={{marginBottom:"69px"}}></iframe>
           </div>
         ) : <p>No Data Available</p>}
+        
+        <div className="overlayDiv1"></div>
+        <div className="overlayDiv2"></div>
+  
+
       </div>
     </>
   );
